25b439c24f63b9de5e1478a9d57d09a0a6e68975
Zefansstr <zefansstr@users.noreply.github.com>
2026-01-14 17:32:21 +0700

    Fix: Replace next-themes import with custom ThemeProvider in Logo component

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useTheme } from "next-themes";
+import { useTheme } from "@/components/providers/theme-provider";
 import { useEffect, useState } from "react";
 
 type LogoProps = {
